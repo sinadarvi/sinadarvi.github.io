7d6f6baa739db51b19c311af18455d2a69545f29
Sina-Darvishi <sinadarvi@gmail.com>
2018-07-16 03:50:38 +0430

    sinadarvi.github.io v0.2.1 - Material Toolbar added and modified, not final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from "./material";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
