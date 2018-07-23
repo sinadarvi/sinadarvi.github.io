a101631e4958c0911c5c0238086ab7bb80588144
Sina-Darvishi <sinadarvi@gmail.com>
2018-07-23 22:57:44 +0430

    sinadarvi.github.io v0.4.2 - my avatar added, background image added, header and footer color modified, about me button added, name onclick removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {AfterViewInit, Component, ElementRef} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements AfterViewInit{
   title = 'Sina Darvishi';
 
@@ -14,6 +15,12 @@ export class AppComponent implements AfterViewInit{
 
   ngAfterViewInit(): void {
     //This will change the background color
-    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#F5F5F5';
+    // this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#F5F5F5';
+    // this.elementRef.nativeElement.ownerDocument.body.style.backgroundImage = Url('https://images.unsplash.com/photo-1529920300777-27eec6beaa41?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=42b9a09204cc170c653a63cd132bbe7a&auto=format&fit=crop&w=1051&q=80');
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundImage = 'url(' + 'https://images.unsplash.com/photo-1529920300777-27eec6beaa41?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=42b9a09204cc170c653a63cd132bbe7a&auto=format&fit=crop&w=1951&q=80' + ')';
+    this.elementRef.nativeElement.ownerDocument.body.style.height = '100%';
+    // this.elementRef.nativeElement.ownerDocument.body.style.backgroundPosition = 'center';
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundRepeat = 'no-repeat';
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundSize = 'cover';
   }
 }
